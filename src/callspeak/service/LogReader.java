1f79f8f57bdd855b2563bcf10d19c95f980eddec
Adrian Pulbere <apulbere@pentalog.fr>
2017-05-01 19:30:46 +0300

    added pom for junit lib

--- src/callspeak/service/LogReader.java
+++ src/callspeak/service/LogReader.java
@@ -1,12 +1,12 @@
 package callspeak.service;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import callspeak.model.Interval;
@@ -16,7 +16,7 @@ public class LogReader {
 	public List<Interval> getLogs(String filePath) {
 		List<Interval> list = emptyList();
 		try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
-		    list = lines.map(Interval::new).collect(Collectors.toList());
+		    list = lines.map(Interval::new).collect(toList());
 		} catch (IOException e) {
 		    //log exception
 		}
